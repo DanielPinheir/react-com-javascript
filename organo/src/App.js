c7c3b6432e5e7b22442f98f8776823cb02111ef6
Daniel Pinheiro Machado <dyelzim@gmail.com>
2023-05-01 01:33:52 -0300

    create cards finish

--- organo/src/App.js
+++ organo/src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Banner from "./componentes/Banner";
 import Formulario from "./componentes/Formulario";
 import Time from "./componentes/Time";
+import Rodape from "./componentes/Rodape";
 
 function App() {
   const times = [
@@ -44,7 +45,7 @@ function App() {
   const [colaboradores, setColaboradores] = useState([]);
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    console.log(colaborador);
+    debugger;
     setColaboradores([...colaboradores, colaborador]);
   };
 
@@ -63,8 +64,12 @@ function App() {
           nome={time.nome}
           corPrimaria={time.corPrimaria}
           corSecundaria={time.corSecundaria}
+          colaboradores={colaboradores.filter(
+            (colaborador) => colaborador.time === time.nome
+          )}
         />
       ))}
+      <Rodape />
     </div>
   );
 }
